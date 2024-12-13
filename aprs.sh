32ba6c7a1e0cf8346e9ff6f817c96ef16f468871
Kenneth Daniel <kenndaniel@yahoo.com>
2024-12-13 11:04:47 -0500

    Corrected SI5351 interface link

--- aprs.sh
+++ aprs.sh
@@ -3,5 +3,5 @@ ln -s /home/ken/Arduino/projects/K9YO_APRS2/APRSFormat.h APRSFormat.h
 ln -s /home/ken/Arduino/projects/K9YO_APRS2/APRSLoop.h APRSLoop.h 
 ln -s /home/ken/Arduino/projects/K9YO_APRS2/APRSSendPkt.h APRSSendPkt.h 
 ln -s /home/ken/Arduino/projects/K9YO_APRS2/geofence.h geofence.h 
-ln -s /home/ken/Arduino/projects/K9YO_APRS2/SI5351InterfaceVHF-16QFN.h SI5351InterfaceVHF-16QFN.h 
+ln -s /home/ken/Arduino/projects/K9YO_APRS2/SI5351InterfaceVHF.h SI5351InterfaceVHF.h 
 
